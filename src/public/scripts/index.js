d8787db3dcd457273a161ad157bf5c32502a77e8
Tim Saunders <tim@sauntimo.org>
2020-10-03 12:10:39 +0100

    Update frontend

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,21 +1,36 @@
+const attatchEventListener = () => {
+  const btn = document.getElementById('makeRequest');
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('inlineFormInput').value;
+    getCount(name);
+  })
+}
 
-const hitApi = async (name) => {
-  const response = await fetch(`/name-counter/${name}`, {
-    dataType: 'json',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  });
+
+const getCount = async (name) => {
+  const response = await fetch(`/name-counter/${name}`);
+  // , {
+  //   dataType: 'json',
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json'
+  //   }
+  // });
 
   const data = await response.json();
 
   const el = document.createElement('div');
-  el.innerHTML = `<div class="alert alert-info fade show" role="alert">
-    <strong>Example:</strong> Name: ${data.name} Count: ${data.count}
+  el.innerHTML = `<div id="result" class="alert alert-info fade show" role="alert">
+    <strong>Result:</strong> Name: ${data.name} Count: ${data.count}
   </div>`;
 
+  const result = document.getElementById('result');
+  if (result) {
+    result.parentNode.removeChild(result);
+  }
   document.getElementsByClassName('container')[0].appendChild(el);
 };
 
-hitApi('oliver');
+// hitApi('oliver');
+attatchEventListener();
