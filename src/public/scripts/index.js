4113b4e78be5eeba113f6d224ae62a8a2ea31b28
Tim Saunders <tim@sauntimo.org>
2020-10-03 12:11:04 +0100

    Tidy up script

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -10,27 +10,16 @@ const attatchEventListener = () => {
 
 const getCount = async (name) => {
   const response = await fetch(`/name-counter/${name}`);
-  // , {
-  //   dataType: 'json',
-  //   headers: {
-  //     'Accept': 'application/json',
-  //     'Content-Type': 'application/json'
-  //   }
-  // });
-
   const data = await response.json();
-
-  const el = document.createElement('div');
-  el.innerHTML = `<div id="result" class="alert alert-info fade show" role="alert">
-    <strong>Result:</strong> Name: ${data.name} Count: ${data.count}
-  </div>`;
-
   const result = document.getElementById('result');
   if (result) {
     result.parentNode.removeChild(result);
   }
+  const el = document.createElement('div');
+  el.innerHTML = `<div id="result" class="alert alert-info fade show" role="alert">
+    <strong>Result:</strong> Name: ${data.name} Count: ${data.count}
+  </div>`;
   document.getElementsByClassName('container')[0].appendChild(el);
 };
 
-// hitApi('oliver');
 attatchEventListener();
